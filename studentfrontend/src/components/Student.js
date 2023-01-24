0b518218a8ace04441ac4338e8c2d0b0912ebb71
Kien Do <kienmarkdo@gmail.com>
2023-01-23 20:28:30 -0500

    Added Add Student feature

--- studentfrontend/src/components/Student.js
+++ studentfrontend/src/components/Student.js
@@ -1,11 +1,36 @@
 import * as React from "react";
-import { Box, TextField, Paper } from "@mui/material";
+import { Box, TextField, Paper, Button } from "@mui/material";
 import { useState } from "react";
 
 export default function Student() {
   const paperStyle = { padding: "50px 20px", width: 600, margin: "20px auto" };
   const [name, setName] = useState("");
   const [address, setAddress] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const student = { name, address };
+    console.log(student.name + " " + student.address);
+
+    fetch("http://localhost:8080/student/add", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(student),
+    })
+      .then(() => {
+        console.log("New student has been added!");
+        alert(
+          "The student, " +
+            student.name +
+            ", with address " +
+            address +
+            " has been added successfully!"
+        );
+      })
+      .catch(() => {
+        console.log("ERROR: Failed to add student");
+      });
+  };
 
   return (
     <Paper elevation={3} style={paperStyle}>
@@ -38,6 +63,9 @@ export default function Student() {
             setAddress(e.target.value);
           }}
         />
+        <Button variant="contained" color="success" onClick={handleSubmit}>
+          Submit
+        </Button>
       </Box>
     </Paper>
   );

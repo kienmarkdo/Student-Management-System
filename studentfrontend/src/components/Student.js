ff5badabed604752e9edd42f4bd979f77743cf61
Kien Do <kienmarkdo@gmail.com>
2023-01-23 20:49:16 -0500

    Finished Add Student feature

--- studentfrontend/src/components/Student.js
+++ studentfrontend/src/components/Student.js
@@ -1,11 +1,12 @@
 import * as React from "react";
 import { Box, TextField, Paper, Button } from "@mui/material";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export default function Student() {
   const paperStyle = { padding: "50px 20px", width: 600, margin: "20px auto" };
   const [name, setName] = useState("");
   const [address, setAddress] = useState("");
+  const [allStudents, setAllStudents] = useState([]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -32,41 +33,66 @@ export default function Student() {
       });
   };
 
+  useEffect(() => {
+    fetch("http://localhost:8080/student/getAll")
+      .then((res) => res.json())
+      .then((jsonRes) => setAllStudents(jsonRes));
+  }, []);
+
   return (
-    <Paper elevation={3} style={paperStyle}>
-      <h1 style={{ color: "#03a9f4" }}>Add Student</h1>
-      <Box
-        component="form"
-        sx={{
-          "& > :not(style)": { m: 1 },
-        }}
-        noValidate
-        autoComplete="off"
-      >
-        <TextField
-          id="outlined-basic"
-          label="Student Name"
-          variant="outlined"
-          fullWidth
-          value={name}
-          onChange={(e) => {
-            setName(e.target.value);
+    <div>
+      <Paper elevation={3} style={paperStyle}>
+        <h1>Add Student</h1>
+        <Box
+          component="form"
+          sx={{
+            "& > :not(style)": { m: 1 },
           }}
-        />
-        <TextField
-          id="outlined-basic"
-          label="Student Address"
-          variant="outlined"
-          fullWidth
-          value={address}
-          onChange={(e) => {
-            setAddress(e.target.value);
-          }}
-        />
-        <Button variant="contained" color="success" onClick={handleSubmit}>
-          Submit
-        </Button>
-      </Box>
-    </Paper>
+          noValidate
+          autoComplete="off"
+        >
+          <TextField
+            id="outlined-basic"
+            label="Student Name"
+            variant="outlined"
+            fullWidth
+            value={name}
+            onChange={(e) => {
+              setName(e.target.value);
+            }}
+          />
+          <TextField
+            id="outlined-basic"
+            label="Student Address"
+            variant="outlined"
+            fullWidth
+            value={address}
+            onChange={(e) => {
+              setAddress(e.target.value);
+            }}
+          />
+          <Button variant="contained" color="success" onClick={handleSubmit}>
+            Submit
+          </Button>
+        </Box>
+      </Paper>
+
+      <Paper elevation={3} style={paperStyle}>
+        <h1>Student List</h1>
+        {allStudents.map((student) => (
+          <Paper
+            elevation={3}
+            style={{ margin: "10px", padding: "15px", textAlign: "left" }}
+            key={student.id}
+          >
+            <strong>Id:</strong> {student.id}
+            <br />
+            <strong>Name:</strong> {student.name}
+            <br />
+            <strong>Address:</strong> {student.address}
+          </Paper>
+        ))}
+      </Paper>
+    </div>
   );
 }
